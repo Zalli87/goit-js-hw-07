55de1c821ae6885a55b9cf9b498a10453cbb90a9
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2022-11-13 16:49:46 +0200

    додає виконання першого завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const galleryItem = createGalleryItemMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML('afterbegin', galleryItem);
+
+galleryEl.addEventListener('click', onGalleryItemClick);
+
+
+function onGalleryItemClick(event) {
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show()
+}
+
+
+function createGalleryItemMarkup(items) {
+    return items.map(({original, preview, description}) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('');
+}
+
+
+
